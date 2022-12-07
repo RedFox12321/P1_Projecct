71b4289702e907ceb75c0b4df12ab1e94b8423ba
Gameiro <alexandrefgame@gmail.com>
2022-12-07 15:39:37 +0000

    Added to_do file and added struct and variables to main

--- Projeto_P1/main.c
+++ Projeto_P1/main.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <string.h>
 #include "funcoes_auxiliares.c"
+
 typedef struct{
     int dia;
     int mes;
@@ -16,27 +17,17 @@ typedef struct{
     char CPU[20];
 } Portatil;
 
-int
-numPE=0, //Portáteis Existentes
-numPD=0, //Portáteis Disponíveis
-numTRE=0, //Total de Requisições Efetuadas
-numRA=0; //Requisições ativas
 
-void menu();
+char menu();
 
 int main()
 {
-    lerInteiro("Indique um numero", 0, 50);
-    menu();
+    int
+    numPE=0, //Portáteis Existentes
+    numPD=0, //Portáteis Disponíveis
+    numTRE=0, //Total de Requisições Efetuadas
+    numRA=0; //Requisições ativas
+
     return 0;
 }
 
-void menu()
-{
-    printf("\t\t\tComputadores portateis\n"
-           "Numero total de portateis - %d\n"
-           "Numero de portateis disponiveis - %d\n"
-           "Numero total de requisicoes efetuadas - %d\n"
-           "Numero de requisicoes ativas - %d\n"
-           , numPE, numPD, numTRE, numRA);
-}
